e976aeeb6da0421d460d8c15633b4d954942b920
Gorun <307532500@qq.com>
2024-06-06 15:02:56 +0800

    修复了未使用主题颜色的问题

--- app/src/main/java/com/tiyi/tiyi_app/ui/LoginPage.kt
+++ app/src/main/java/com/tiyi/tiyi_app/ui/LoginPage.kt
@@ -1,5 +1,6 @@
 package com.tiyi.tiyi_app.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
@@ -26,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,7 +55,9 @@ fun LoginBlock(onLoginClick: (result: LoginInfo) -> Unit) {
     var username by remember { mutableStateOf(TextFieldValue("")) }
     var password by remember { mutableStateOf(TextFieldValue("")) }
     Box(
-        modifier = Modifier.background(Color.White)
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .clip(RoundedCornerShape(16.dp))
     ) {
         Column(
             modifier = Modifier
@@ -79,7 +84,6 @@ fun LoginBlock(onLoginClick: (result: LoginInfo) -> Unit) {
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.White)
                     .padding(8.dp)
             )
             Spacer(modifier = Modifier.height(8.dp))
@@ -97,7 +101,6 @@ fun LoginBlock(onLoginClick: (result: LoginInfo) -> Unit) {
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.White)
                     .padding(8.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
@@ -112,7 +115,8 @@ fun LoginBlock(onLoginClick: (result: LoginInfo) -> Unit) {
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun LoginBlockPreview() {
     TiYiAppTheme {
         Surface {
