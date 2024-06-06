afe345d94efdb4b851fd359ec5a90b336cdb1777
Gorun <307532500@qq.com>
2024-06-06 15:48:18 +0800

    添加密码输入时隐藏

--- app/src/main/java/com/tiyi/tiyi_app/ui/LoginPage.kt
+++ app/src/main/java/com/tiyi/tiyi_app/ui/LoginPage.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -146,6 +147,7 @@ fun LoginBlock(onLoginClick: (result: LoginInfo) -> Unit) {
                 trailingIcon = {
                     Icon(Icons.Outlined.Cancel, contentDescription = "清空")
                 },
+                visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
