5a20a5950cc2e83895068089053cce3043cafd36
Gorun <307532500@qq.com>
2024-06-07 10:25:35 +0800

    添加基本搜索框

---
+++ app/src/main/java/com/tiyi/tiyi_app/ui/RecentPage.kt
@@ -0,0 +1,58 @@
+package com.tiyi.tiyi_app.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tiyi.tiyi_app.ui.theme.TiYiAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecentPage(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        SearchBar(
+            query = "",
+            onQueryChange = { },
+            onSearch = { },
+            active = false,
+            onActiveChange = {},
+            leadingIcon = {
+                Icon(Icons.Outlined.Menu, contentDescription = "菜单")
+            },
+            trailingIcon = {
+                Icon(Icons.Outlined.Search, contentDescription = "搜索")
+            },
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+
+            Text(text = "Recent")
+        }
+    }
+}
+
+@Composable
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun RecentPagePreview() {
+    TiYiAppTheme {
+        Surface {
+            RecentPage(modifier = Modifier)
+        }
+    }
+}
