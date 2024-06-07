bdf2d40f4d3071bfa3b330343eeba0bb85df5392
Hugo Ho <hugoho912@outlook.com>
2024-06-07 12:37:12 +0800

    限制文件类型为 audio

--- app/src/main/java/com/tiyi/tiyi_app/screen/AnalysisPage.kt
+++ app/src/main/java/com/tiyi/tiyi_app/screen/AnalysisPage.kt
@@ -5,21 +5,32 @@ import android.content.Intent
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
-import androidx.compose.runtime.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -33,7 +44,7 @@ import androidx.compose.ui.unit.sp
 
 @Preview(name = "AnalysisPage", showSystemUi = true)
 @Composable
-fun AnalysisPage(modifier: Modifier = Modifier.fillMaxSize()) {
+fun AnalysisPage(modifier: Modifier = Modifier) {
     FileUploadUI()
 }
 
@@ -77,7 +88,7 @@ fun FileUploadUI() {
                     }
                     .clickable {
                         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-                            type = "*/*"
+                            type = "audio/*"
                         }
                         filePickerLauncher.launch(intent)
                     },
