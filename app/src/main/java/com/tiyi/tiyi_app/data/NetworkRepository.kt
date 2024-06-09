a33fedd33c0dd455c4f8cf98e268f27c5237f01d
SimpleMaxR <25173193+SimpleMaxR@users.noreply.github.com>
2024-06-09 16:08:59 +0800

    Refactor: rename UserRepository to NetworkRepository

---
+++ app/src/main/java/com/tiyi/tiyi_app/data/NetworkRepository.kt
@@ -0,0 +1,99 @@
+package com.tiyi.tiyi_app.data
+
+import com.tiyi.tiyi_app.dto.AnalysisItemsModel
+import com.tiyi.tiyi_app.dto.AudioDetailsModel
+import com.tiyi.tiyi_app.dto.AudioListModel
+import com.tiyi.tiyi_app.dto.AudioUploadResponseModel
+import com.tiyi.tiyi_app.dto.LoginRequest
+import com.tiyi.tiyi_app.dto.RegisterRequest
+import com.tiyi.tiyi_app.network.MusicApiService
+import com.tiyi.tiyi_app.dto.CommonResponseModel
+import com.tiyi.tiyi_app.dto.ConsumptionCheckModel
+import com.tiyi.tiyi_app.dto.FileResponseModel
+import com.tiyi.tiyi_app.dto.LabelRequest
+import com.tiyi.tiyi_app.dto.ListTagModel
+import com.tiyi.tiyi_app.dto.RechargeItemsModel
+import com.tiyi.tiyi_app.dto.UserDetailsModel
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+
+@Suppress("unused")
+class NetworkRepository(private val apiService: MusicApiService) {
+
+    // User API
+    suspend fun loginUser(loginRequest: LoginRequest): CommonResponseModel {
+        return apiService.loginUser(loginRequest)
+    }
+
+    suspend fun registerUser(registerRequest: RegisterRequest): CommonResponseModel {
+        return apiService.registerUser(registerRequest)
+    }
+
+    suspend fun getUserDetails(): UserDetailsModel {
+        return apiService.getUserDetails()
+    }
+
+    // Music API
+    suspend fun uploadAudio(file: RequestBody): AudioUploadResponseModel {
+        return apiService.uploadAudio(file)
+    }
+
+    suspend fun labelAudio(labelRequest: LabelRequest): CommonResponseModel {
+        return apiService.labelAudio(labelRequest)
+    }
+
+    suspend fun getAudioDetails(audioId: String): AudioDetailsModel {
+        return apiService.getAudioDetails(audioId)
+    }
+
+    suspend fun deleteAudio(audioId: String): CommonResponseModel {
+        return apiService.deleteAudio(audioId)
+    }
+
+    suspend fun listAudios(name: String?, tags: String?): AudioListModel {
+        return apiService.listAudios(name, tags)
+    }
+
+    // Tag API
+    suspend fun addTag(tagName: String): CommonResponseModel {
+        return apiService.addTag(tagName)
+    }
+
+    suspend fun listTags(): ListTagModel {
+        return apiService.listTags()
+    }
+
+    suspend fun deleteTag(tagName: String): ListTagModel {
+        return apiService.deleteTag(tagName)
+    }
+
+    // Coin API
+    suspend fun getOrderInfo(rechargeId: String): CommonResponseModel {
+        return apiService.getOrderInfo(rechargeId)
+    }
+
+    suspend fun getAnalysisItems(): AnalysisItemsModel {
+        return apiService.getAnalysisItems()
+    }
+
+    suspend fun getRechargeItems(): RechargeItemsModel {
+        return apiService.getRechargeItems()
+    }
+
+    suspend fun checkMusicCoinConsumption(itemNames: String): ConsumptionCheckModel {
+        return apiService.checkMusicCoinConsumption(itemNames)
+    }
+
+    suspend fun getOrderInfoStr(rechargeId: String): CommonResponseModel {
+        return apiService.getOrderInfoStr(rechargeId)
+    }
+
+    // File API
+    suspend fun uploadFile(file: RequestBody): FileResponseModel {
+        return apiService.uploadFile(file)
+    }
+
+    suspend fun downloadFile(filename: String): ResponseBody {
+        return apiService.downloadFile(filename)
+    }
+}
