8a4b6feb4c1ac3b62f1d2329a6d0fa85e6005779
Gorun <307532500@qq.com>
2024-06-10 16:49:56 +0800

    细分Result类型

--- app/src/main/java/com/tiyi/tiyi_app/pojo/Result.kt
+++ app/src/main/java/com/tiyi/tiyi_app/pojo/Result.kt
@@ -1,13 +1,19 @@
 package com.tiyi.tiyi_app.pojo
 
+import retrofit2.HttpException
+import java.net.ConnectException
+
 sealed class Result<out T> {
     data class Success<out T>(val data: T) : Result<T>()
     // Code 400
-    data class BadRequest(val exception: Exception) : Result<Nothing>()
+    data class BadRequest(val exception: HttpException) : Result<Nothing>()
     // Code 401
-    data class Unauthorized(val exception: Exception) : Result<Nothing>()
+    data class Unauthorized(val exception: HttpException) : Result<Nothing>()
     // Code 500
-    data class ServerInternalError(val exception: Exception) : Result<Nothing>()
+    data class ServerInternalError(val exception: HttpException) : Result<Nothing>()
     // Not connected
-    data class NetworkError(val exception: Exception) : Result<Nothing>()
+    data class NetworkError(val exception: ConnectException) : Result<Nothing>()
 }
+
+// Not as promised in the API document
+class CorruptedApiException : RuntimeException()
