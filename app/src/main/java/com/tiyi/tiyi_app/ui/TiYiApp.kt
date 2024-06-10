65826d149b5f2418471f1940b42b1b53292a331a
Gorun <307532500@qq.com>
2024-06-10 19:20:00 +0800

    修改Loading的位置为在状态栏上方

--- app/src/main/java/com/tiyi/tiyi_app/ui/TiYiApp.kt
+++ app/src/main/java/com/tiyi/tiyi_app/ui/TiYiApp.kt
@@ -1,5 +1,6 @@
 package com.tiyi.tiyi_app.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,17 +9,20 @@ import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,6 +32,7 @@ import com.tiyi.tiyi_app.page.AnalysisPage
 import com.tiyi.tiyi_app.page.LoginScreen
 import com.tiyi.tiyi_app.page.ProfilePage
 import com.tiyi.tiyi_app.page.RecentPage
+import com.tiyi.tiyi_app.viewModel.MainViewModel
 
 @Preview
 @Composable
@@ -49,6 +54,7 @@ fun TiYiApp() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    val mainViewModel: MainViewModel = viewModel()
 
     val menuData = listOf(
         BottomItemData("最近文件", Icons.Outlined.CheckCircle, "RecentPage"),
@@ -57,7 +63,15 @@ fun MainScreen(modifier: Modifier = Modifier) {
     )
 
     Scaffold(modifier = modifier.fillMaxSize(), bottomBar = {
-        BottomNavigationBar(navController = navController, items = menuData)
+        Column {
+            val loading by mainViewModel.loading.collectAsState()
+            if (loading) {
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            BottomNavigationBar(navController = navController, items = menuData)
+        }
     }) { innerPadding ->
         NavHost(
             navController = navController,
