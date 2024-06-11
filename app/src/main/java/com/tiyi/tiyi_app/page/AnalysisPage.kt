61c72b36af2db485178b9e30af431ea2c4232c7f
Gorun <307532500@qq.com>
2024-06-11 11:44:57 +0800

    添加分析项目UI

--- app/src/main/java/com/tiyi/tiyi_app/page/AnalysisPage.kt
+++ app/src/main/java/com/tiyi/tiyi_app/page/AnalysisPage.kt
@@ -1,18 +1,39 @@
 package com.tiyi.tiyi_app.page
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Paid
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tiyi.tiyi_app.ui.theme.TiYiAppTheme
 
 
@@ -20,6 +41,77 @@ import com.tiyi.tiyi_app.ui.theme.TiYiAppTheme
 @Composable
 fun AnalysisPage(modifier: Modifier) {
 
+}
+
+@Composable
+fun AnalysisItem(
+    analysisName: String,
+    analysisDescription: String,
+    checked: Boolean,
+    price: Int,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    extendedControl: @Composable (
+        modifier: Modifier
+    ) -> Unit = {},
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+            .animateContentSize()
+    ) {
+        Column {
+            ListItem(
+                headlineContent = {
+                    Text(
+                        text = analysisName,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                supportingContent = {
+                    Text(
+                        text = analysisDescription,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                trailingContent = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Paid,
+                            contentDescription = "Localized description"
+                        )
+                        Text(
+                            text = "$price",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                        Checkbox(
+                            checked,
+                            onCheckedChange,
+                        )
+                    }
+                },
+                colors = ListItemDefaults.colors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                ),
+                modifier = Modifier
+                    .clickable { onCheckedChange(!checked) }
+            )
+            AnimatedVisibility(
+                visible = checked,
+            ) {
+                extendedControl(
+                    Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,5 +157,36 @@ fun AnalysisAppBarPreview() {
             AnalysisAppBar(sliceName = "Analysis Slice")
             AnalysisAppBar(sliceName = "Analysis Slice ".repeat(5))
         }
+    }
+}
+
+@Composable
+@Preview(name = "ListItem - Light")
+@Preview(name = "ListItem - Dark", uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
+fun AnalysisItemPreview() {
+    TiYiAppTheme {
+        var checked by remember { mutableStateOf(true) }
+        var sliderPosition by remember { mutableFloatStateOf(0f) }
+        AnalysisItem(
+            analysisName = "BPM",
+            analysisDescription = "Beats Per Minutes 每分钟的节拍数",
+            checked = checked,
+            price = 10,
+            onCheckedChange = { checked = !checked },
+            extendedControl = { modifier ->
+                Slider(
+                    value = sliderPosition,
+                    onValueChange = { sliderPosition = it },
+                    valueRange = 0f..100f,
+                    colors = SliderDefaults.colors(
+                        thumbColor = MaterialTheme.colorScheme.primary,
+                        activeTrackColor = MaterialTheme.colorScheme.primary,
+                        inactiveTrackColor = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f),
+                    ),
+                    modifier = modifier
+                )
+            },
+            modifier = Modifier.safeDrawingPadding()
+        )
     }
 }
