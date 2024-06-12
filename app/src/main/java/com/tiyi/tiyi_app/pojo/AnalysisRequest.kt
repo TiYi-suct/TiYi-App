229f648405f5e042259505366bc834c9c1d67d85
Gorun <307532500@qq.com>
2024-06-12 16:06:08 +0800

    添加AnalysisRequest来获取用户的请求信息

--- app/src/main/java/com/tiyi/tiyi_app/pojo/AnalysisRequest.kt
+++ app/src/main/java/com/tiyi/tiyi_app/pojo/AnalysisRequest.kt
@@ -1,5 +1,7 @@
 package com.tiyi.tiyi_app.pojo
 
+import java.io.Serializable
+
 val analysisUrlOfId = mapOf(
     1 to "/analysis/bpm",
     2 to "/analysis/transposition",
@@ -10,4 +12,9 @@ val analysisUrlOfId = mapOf(
 
 data class AnalysisRequest(
     val audioId: String,
-)
+    val analysisItems: Map<AnalysisItemInfo, Boolean>,
+    val transpositionSteps: Int? = null,
+    val mfccFactor: Int? = null,
+    val startTime: Float? = null,
+    val endTime: Float? = null
+) : Serializable
