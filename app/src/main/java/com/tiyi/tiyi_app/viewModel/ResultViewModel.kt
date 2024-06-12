36b20c45cec205c2e7bbcb4ade5788e357c5e8cd
Gorun <307532500@qq.com>
2024-06-12 15:53:52 +0800

    添加ResultActivity作为启动项

--- app/src/main/java/com/tiyi/tiyi_app/viewModel/ResultViewModel.kt
+++ app/src/main/java/com/tiyi/tiyi_app/viewModel/ResultViewModel.kt
@@ -7,16 +7,16 @@ import com.tiyi.tiyi_app.pojo.AnalysisResult
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class ResultViewModel(
-    val application: Application
-): AndroidViewModel(application){
+    application: Application
+) : AndroidViewModel(application) {
     companion object {
         private const val TAG = "ResultViewModel"
     }
+
     private val tiyiApplication = application as TiyiApplication
     val networkRepository = tiyiApplication.networkRepository
 
     private val _analysisResult = MutableStateFlow(AnalysisResult())
     val analysisResult = _analysisResult
-
 
 }
