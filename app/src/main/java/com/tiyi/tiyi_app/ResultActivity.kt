cb4e6249c96b6b8d19b828b34284c060dd3674b6
Gorun <307532500@qq.com>
2024-06-12 16:19:32 +0800

    添加点击分析跳转到ResultActivity的功能

--- app/src/main/java/com/tiyi/tiyi_app/ResultActivity.kt
+++ app/src/main/java/com/tiyi/tiyi_app/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.tiyi.tiyi_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +18,7 @@ class ResultActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d("test", "onCreate: Created")
         setContent {
             TiYiAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
